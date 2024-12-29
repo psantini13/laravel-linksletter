697c0d5f18b54d383db6d6bc32974253278a9b7f
Pablo Santini <pjsantini@gmail.com>
2024-12-29 17:49:29 -0600

    Larastan Level 9 run - resolves #10

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -6,11 +6,14 @@ use App\Http\Requests\StoreLinkRequest;
 use App\Http\Requests\UpdateLinkRequest;
 use App\Models\Link;
 use App\Models\User;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
 
 class LinkController extends Controller
 {
-    public function index()
-    {
+    public function index(): View|Factory|Application {
         $links = Link::query()
             // Filter by tenant id (user_id)
             ->where('user_id', auth()->id())
@@ -22,8 +25,7 @@ class LinkController extends Controller
         ]);
     }
 
-    public function create()
-    {
+    public function create(): View|Factory|Application {
         $users = User::all();
 
         return view('links.create', [
@@ -31,8 +33,7 @@ class LinkController extends Controller
         ]);
     }
 
-    public function store(StoreLinkRequest $request)
-    {
+    public function store(StoreLinkRequest $request): RedirectResponse {
         $link = Link::create(
             $request->validated() + [
                 'user_id' => auth()->id(),
@@ -41,6 +42,7 @@ class LinkController extends Controller
 
         // If there is no position, set it to the last
         if (! $link->position) {
+            // @phpstan-ignore-next-line
             $link->position = Link::max('position') + 1;
             $link->save();
         }
@@ -49,8 +51,7 @@ class LinkController extends Controller
             ->with('message', 'Link created successfully.');
     }
 
-    public function edit(Link $link)
-    {
+    public function edit(Link $link): View|Factory|Application {
         // Check if user is the owner of the link
         abort_unless($link->user_id === auth()->id(), 404);
 
@@ -62,8 +63,7 @@ class LinkController extends Controller
         ]);
     }
 
-    public function update(UpdateLinkRequest $request, Link $link)
-    {
+    public function update(UpdateLinkRequest $request, Link $link): RedirectResponse {
         abort_unless($link->user_id === auth()->id(), 404);
 
         $link->update($request->validated());
@@ -72,8 +72,7 @@ class LinkController extends Controller
             ->with('message', 'Link updated successfully.');
     }
 
-    public function destroy(Link $link)
-    {
+    public function destroy(Link $link): RedirectResponse {
         abort_unless($link->user_id === auth()->id(), 404);
 
         $link->delete();
